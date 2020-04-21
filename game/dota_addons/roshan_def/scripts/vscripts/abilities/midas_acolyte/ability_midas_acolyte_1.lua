019d26e1ced7ebcaebdeabae4b3776c43418c9eb
Vitaliy <vulkantsk@gmail.com>
2020-04-22 03:14:15 +0700

    fixed maxim code = ?

--- game/dota_addons/roshan_def/scripts/vscripts/abilities/midas_acolyte/ability_midas_acolyte_1.lua
+++ game/dota_addons/roshan_def/scripts/vscripts/abilities/midas_acolyte/ability_midas_acolyte_1.lua
@@ -1,9 +1,13 @@
-ability_midas_acolyte_1 = class({
-	GetIntrinsicModifierName = function(self) return 'modifier_ability_midas_acolyte_1_passive' end,
-})
-
 LinkLuaModifier("modifier_ability_midas_acolyte_1_bonus_dmg", 'abilities/midas_acolyte/ability_midas_acolyte_1.lua', LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_ability_midas_acolyte_1_passive", 'abilities/midas_acolyte/ability_midas_acolyte_1.lua', LUA_MODIFIER_MOTION_NONE)
+
+
+ability_midas_acolyte_1 = class({})
+
+function ability_midas_acolyte_1:GetIntrinsicModifierName()
+	return 'modifier_ability_midas_acolyte_1_passive'
+end
+
 modifier_ability_midas_acolyte_1_bonus_dmg = class({
     IsHidden                = function(self) return true end,
     IsPurgable              = function(self) return false end,
@@ -21,59 +25,42 @@ modifier_ability_midas_acolyte_1_passive = class({
     IsBuff                  = function(self) return true end,
     RemoveOnDeath           = function(self) return true end,
     DeclareFunctions		= function(self) return {MODIFIER_EVENT_ON_ATTACK_LANDED} end,
-    OnCreated 				= function(self)
+})
+
+function modifier_ability_midas_acolyte_1_passive:OnCreated()
     	self.parent = self:GetParent()
     	self.ability = self:GetAbility()
-    	self.radius = self.ability:GetLevelSpecialValueFor('radius',1)
-    	self.bonus_health = self.ability:GetLevelSpecialValueFor('bonus_health',1)
-    	self.bonus_dmg = self.ability:GetLevelSpecialValueFor('bonus_dmg',1)
-    end,
-    OnAttackLanded 			= function(self,data)
-    	if self.ability:IsCooldownReady() and data.target == self.parent and self.ability:GetLevel() > 0 then 
-    		self:OnSpellStart()
-    		self.ability:StartCooldown(self.ability:GetCooldown(self.ability:GetLevel()))
-    	end
-    end,
+    	self.radius = self.ability:GetSpecialValueFor('radius')
+    	self.bonus_health = self.ability:GetSpecialValueFor('bonus_health')
+    	self.bonus_dmg = self.ability:GetSpecialValueFor('bonus_dmg')
+end
 
-	OnSpellStart = function(self)
-		local caster = self.parent
-		local units = FindUnitsInRadius(caster:GetTeamNumber(), 
-		caster:GetAbsOrigin(),
-		nil,
-		self.radius,
-		DOTA_UNIT_TARGET_TEAM_BOTH,
-		DOTA_UNIT_TARGET_CREEP, 
-		DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE,
-		FIND_CLOSEST, 
-		false)
-		for k,v in pairs(units) do
-			if v:GetOwnerEntity() or v:IsBoss() then 
-				table.remove(units,k)
-			end
-		end
-		PrintTable(units)
-		if #units < 1 then return end
-		caster:EmitSound("DOTA_Item.Hand_Of_Midas")
-		for k,v in pairs(units) do
-			self:MidasActivate(v)
-		end
-		local bonusHealth = #units * self.bonus_health
-		local maxHealth = caster:GetMaxHealth()
-		caster:SetBaseMaxHealth(maxHealth + bonusHealth)
-		caster:SetMaxHealth(maxHealth + bonusHealth)
-		caster:SetHealth(caster:GetHealth() + bonusHealth)
+function modifier_ability_midas_acolyte_1_passive:OnAttackLanded(data)
+	local target = data.target
+	local attacker = data.attacker
 
-		caster:AddStackModifier({
-			ability = self,
-			modifier = 'modifier_ability_midas_acolyte_1_bonus_dmg',
-			count = #units * self.bonus_dmg,
-			caster = caster,
-		})
-	end,
+	if self.ability:IsCooldownReady() and target == self.parent and not attacker:IsBuilding() and not attacker:IsControllableByAnyPlayer()  then 
+		self:MidasActivate(attacker)
+--    		self.ability:StartCooldown(self.ability:GetCooldown(self.ability:GetLevel()))
+	end
+end
 
-	MidasActivate = function(self,target)
-		local midas_particle = ParticleManager:CreateParticle("particles/items2_fx/hand_of_midas.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)	
-		ParticleManager:SetParticleControlEnt(midas_particle, 1, self.parent, PATTACH_POINT_FOLLOW, "attach_hitloc", self.parent:GetAbsOrigin(), false)
-		target:KillTarget(self.parent, self)
-	end,
-})
+function modifier_ability_midas_acolyte_1_passive:MidasActivate(target)		
+	local caster = self.parent
+	local bonusHealth = self.bonus_health
+	local maxHealth = caster:GetMaxHealth()
+	caster:SetBaseMaxHealth(maxHealth + bonusHealth)
+	caster:SetMaxHealth(maxHealth + bonusHealth)
+	caster:SetHealth(caster:GetHealth() + bonusHealth)
+
+	caster:AddStackModifier({
+		ability = self,
+		modifier = 'modifier_ability_midas_acolyte_1_bonus_dmg',
+		count = self.bonus_dmg,
+		caster = caster,
+	})
+	caster:EmitSound("DOTA_Item.Hand_Of_Midas")
+	local midas_particle = ParticleManager:CreateParticle("particles/items2_fx/hand_of_midas.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)	
+	ParticleManager:SetParticleControlEnt(midas_particle, 1, self.parent, PATTACH_POINT_FOLLOW, "attach_hitloc", self.parent:GetAbsOrigin(), false)
+	target:KillTarget(self.parent, self)
+end
