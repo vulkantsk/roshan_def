5ad87ec65882a0fb613fbd5e85a549b19f6db4ad
fdsatrew <fdsatrewq3@gmail.com>
2020-10-18 15:45:28 +0300

    Второй шанс. При смерти будет реинкарнирован через 3 секунды с 30% ХП. Сработает только 1 раз.

---
+++ game/dota_addons/roshan_def/scripts/vscripts/abilities/roshan/roshan_second_chance.lua
@@ -0,0 +1,76 @@
+LinkLuaModifier("modifier_roshdef_roshan_second_chance_buff", "abilities/roshan/roshan_second_chance.lua", LUA_MODIFIER_MOTION_NONE)
+
+roshdef_roshan_second_chance = class({
+    GetIntrinsicModifierName = function()
+        return "modifier_roshdef_roshan_second_chance_buff"
+    end
+})
+
+modifier_roshdef_roshan_second_chance_buff = class({
+    IsHidden = function()
+        return true
+    end,
+    IsPurgable = function()
+        return false
+    end,
+    IsDebuff = function()
+        return false
+    end,
+    RemoveOnDeath = function()
+        return false
+    end,
+    AllowIllusionDuplicate = function()
+        return false
+    end,
+    DeclareFunctions = function()
+        return
+        {
+            MODIFIER_PROPERTY_REINCARNATION,
+            MODIFIER_EVENT_ON_RESPAWN
+        }
+    end,
+    GetAttributes = function()
+        return MODIFIER_ATTRIBUTE_PERMANENT
+    end
+})
+
+function modifier_roshdef_roshan_second_chance_buff:OnCreated()
+    if (not IsServer()) then
+        return
+    end
+    self.ability = self:GetAbility()
+    self.caster = self:GetParent()
+    self.currentProcs = 0
+    self:OnRefresh()
+end
+
+function modifier_roshdef_roshan_second_chance_buff:OnRefresh()
+    if (not IsServer()) then
+        return
+    end
+    self.maxProcs = self.ability:GetSpecialValueFor("max_procs")
+    self.respawnDelay = self.ability:GetSpecialValueFor("respawn_delay")
+    self.respawnHpPercent = self.ability:GetSpecialValueFor("respawn_hp_pct") / 100
+end
+
+function modifier_roshdef_roshan_second_chance_buff:ReincarnateTime()
+    if (not IsServer()) then
+        return
+    end
+    if(self.currentProcs < self.maxProcs) then
+        self.reincarnated = true
+        self.currentProcs = self.currentProcs + 1
+        return self.respawnDelay
+    end
+    self.reincarnated = nil
+    return nil
+end
+
+function modifier_roshdef_roshan_second_chance_buff:OnRespawn(keys)
+    if (not IsServer()) then
+        return
+    end
+    if(keys.unit == self.caster and self.reincarnated) then
+        self.caster:SetHealth(self.caster:GetMaxHealth() * self.respawnHpPercent)
+    end
+end
